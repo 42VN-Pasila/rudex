a9128d3e7e1f45995b9d2a9e516e1a0ed1a5df1e
Siuol <nguyencaotan03072000@gmail.com>
2025-11-11 13:56:53 +0200

    Stop tracking dist directory

--- dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_REFRESH_TOKEN_EXP = exports.JWT_ACCESS_TOKEN_EXP = void 0;
-exports.JWT_ACCESS_TOKEN_EXP = 60 * 60 * 24; // 1 day
-exports.JWT_REFRESH_TOKEN_EXP = 60 * 60 * 24 * 7; // 7 days
-//# sourceMappingURL=constants.js.map
