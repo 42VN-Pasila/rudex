7974e02817656387ad4e5c555d852fd3380c784c
Kha Tieu <tieuduykha1810@gmail.com>
2025-11-11 14:10:37 +0200

    made proper jest test

--- tests/validators/validators.spec.ts
+++ tests/validators/validators.spec.ts
@@ -5,148 +5,135 @@ import {
 } from '@domain/error/validatorError';
 import { ValidateUser } from '@src/valiadators/validateUser';
 
+// Proper Jest patterns using toThrow with message matching and method spies
+
 describe('ValidateUser', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   describe('validateUsername', () => {
     it('Expected Error: Username length must be 8-16.', () => {
+      const spy = jest.spyOn(ValidateUser, 'validateUsername');
       const username = 'user';
-
-      try {
-        ValidateUser.validateUsername(username);
-        throw new Error('Expected to be failed');
-      } catch (err) {
-        if (!(err instanceof ValidateUsernameError)) throw new Error('Wrong type error');
-        expect(err.message).toBe('Username length must be 8-16.');
-      }
+      expect(() => ValidateUser.validateUsername(username)).toThrow(ValidateUsernameError);
+      expect(() => ValidateUser.validateUsername(username)).toThrow(
+        'Username length must be 8-16.'
+      );
+      expect(spy).toHaveBeenCalledWith(username);
     });
 
     it('Expected Error: Username can only contains letters, numbers, or [_.-]', () => {
+      const spy = jest.spyOn(ValidateUser, 'validateUsername');
       const username = '"user" OR 1=1';
-
-      try {
-        ValidateUser.validateUsername(username);
-        throw new Error('Expected to be failed');
-      } catch (err) {
-        if (!(err instanceof ValidateUsernameError)) throw new Error('Wrong type error');
-        expect(err.message).toBe('Username can only contains letters, numbers, or [_.-]');
-      }
+      expect(() => ValidateUser.validateUsername(username)).toThrow(ValidateUsernameError);
+      expect(() => ValidateUser.validateUsername(username)).toThrow(
+        'Username can only contains letters, numbers, or [_.-]'
+      );
+      expect(spy).toHaveBeenCalledWith(username);
     });
 
-    it('Expected Sucess', () => {
+    it('Expected Success', () => {
+      const spy = jest.spyOn(ValidateUser, 'validateUsername');
       const username = 'username';
-
       expect(() => ValidateUser.validateUsername(username)).not.toThrow();
+      expect(spy).toHaveBeenCalledWith(username);
     });
   });
 
   describe('validateEmail', () => {
     it('Expected Error: Email cannot contain whitspace, quote and backflash', () => {
+      const spy = jest.spyOn(ValidateUser, 'validateEmail');
       const email = 'this is email@gmail.com';
-
-      try {
-        ValidateUser.validateEmail(email);
-        throw new Error('Expected to be failed');
-      } catch (err) {
-        if (!(err instanceof ValidateEmailError)) throw new Error('Wrong type error');
-        expect(err.message).toBe('Email cannot contain whitspace, quote and backflash');
-      }
+      expect(() => ValidateUser.validateEmail(email)).toThrow(ValidateEmailError);
+      expect(() => ValidateUser.validateEmail(email)).toThrow(
+        'Email cannot contain whitspace, quote and backflash'
+      );
+      expect(spy).toHaveBeenCalledWith(email);
     });
 
     it('Expected Error: Email must be a valid address (ex: email@gmail.com)', () => {
+      const spy = jest.spyOn(ValidateUser, 'validateEmail');
       const email = 'ft_transecendenceEmail';
-
-      try {
-        ValidateUser.validateEmail(email);
-        throw new Error('Expected to be failed');
-      } catch (err) {
-        if (!(err instanceof ValidateEmailError)) throw new Error('Wrong type error');
-        expect(err.message).toBe('Email must be a valid address (ex: email@gmail.com)');
-      }
+      expect(() => ValidateUser.validateEmail(email)).toThrow(ValidateEmailError);
+      expect(() => ValidateUser.validateEmail(email)).toThrow(
+        'Email must be a valid address (ex: email@gmail.com)'
+      );
+      expect(spy).toHaveBeenCalledWith(email);
     });
 
-    it('Expected Sucess', () => {
+    it('Expected Success', () => {
+      const spy = jest.spyOn(ValidateUser, 'validateEmail');
       const email = 'thisisemail@test.vn';
-
       expect(() => ValidateUser.validateEmail(email)).not.toThrow();
+      expect(spy).toHaveBeenCalledWith(email);
     });
   });
 
   describe('validatePassword', () => {
     it('Expected Error: Password length must be 8-16', () => {
+      const spy = jest.spyOn(ValidateUser, 'validatePassword');
       const password = 'hello';
-
-      try {
-        ValidateUser.validatePassword(password);
-        throw new Error('Expected to be failed');
-      } catch (err) {
-        if (!(err instanceof ValidatePasswordError)) throw new Error('Wrong type error');
-        expect(err.message).toBe('Password length must be 8-16');
-      }
+      expect(() => ValidateUser.validatePassword(password)).toThrow(ValidatePasswordError);
+      expect(() => ValidateUser.validatePassword(password)).toThrow('Password length must be 8-16');
+      expect(spy).toHaveBeenCalledWith(password);
     });
 
     it('Expected Error: Password requires at least 1 lowercase letter', () => {
+      const spy = jest.spyOn(ValidateUser, 'validatePassword');
       const password = 'THISISPASSWORD';
-
-      try {
-        ValidateUser.validatePassword(password);
-        throw new Error('Expected to be failed');
-      } catch (err) {
-        if (!(err instanceof ValidatePasswordError)) throw new Error('Wrong type error');
-        expect(err.message).toBe('Password requires at least 1 lowercase letter');
-      }
+      expect(() => ValidateUser.validatePassword(password)).toThrow(ValidatePasswordError);
+      expect(() => ValidateUser.validatePassword(password)).toThrow(
+        'Password requires at least 1 lowercase letter'
+      );
+      expect(spy).toHaveBeenCalledWith(password);
     });
 
     it('Expected Error: Password requires at least 1 uppercase letter', () => {
+      const spy = jest.spyOn(ValidateUser, 'validatePassword');
       const password = 'thisispassword';
-
-      try {
-        ValidateUser.validatePassword(password);
-        throw new Error('Expected to be failed');
-      } catch (err) {
-        if (!(err instanceof ValidatePasswordError)) throw new Error('Wrong type error');
-        expect(err.message).toBe('Password requires at least 1 uppercase letter');
-      }
+      expect(() => ValidateUser.validatePassword(password)).toThrow(ValidatePasswordError);
+      expect(() => ValidateUser.validatePassword(password)).toThrow(
+        'Password requires at least 1 uppercase letter'
+      );
+      expect(spy).toHaveBeenCalledWith(password);
     });
 
     it('Expected Error: Password requires at least 1 number', () => {
+      const spy = jest.spyOn(ValidateUser, 'validatePassword');
       const password = 'thisisPASSWORD';
-
-      try {
-        ValidateUser.validatePassword(password);
-        throw new Error('Expected to be failed');
-      } catch (err) {
-        if (!(err instanceof ValidatePasswordError)) throw new Error('Wrong type error');
-        expect(err.message).toBe('Password requires at least 1 number');
-      }
+      expect(() => ValidateUser.validatePassword(password)).toThrow(ValidatePasswordError);
+      expect(() => ValidateUser.validatePassword(password)).toThrow(
+        'Password requires at least 1 number'
+      );
+      expect(spy).toHaveBeenCalledWith(password);
     });
 
     it('Expected Error: Password requires at least 1 special character', () => {
+      const spy = jest.spyOn(ValidateUser, 'validatePassword');
       const password = 'th1s1sPASSWORD';
-
-      try {
-        ValidateUser.validatePassword(password);
-        throw new Error('Expected to be failed');
-      } catch (err) {
-        if (!(err instanceof ValidatePasswordError)) throw new Error('Wrong type error');
-        expect(err.message).toBe('Password requires at least 1 special character');
-      }
+      expect(() => ValidateUser.validatePassword(password)).toThrow(ValidatePasswordError);
+      expect(() => ValidateUser.validatePassword(password)).toThrow(
+        'Password requires at least 1 special character'
+      );
+      expect(spy).toHaveBeenCalledWith(password);
     });
 
     it('Expected Error: Password cannot contain quotes, backflash or whitespace', () => {
+      const spy = jest.spyOn(ValidateUser, 'validatePassword');
       const password = 'th1s !s PASSWORD';
-
-      try {
-        ValidateUser.validatePassword(password);
-        throw new Error('Expected to be failed');
-      } catch (err) {
-        if (!(err instanceof ValidatePasswordError)) throw new Error('Wrong type error');
-        expect(err.message).toBe('Password cannot contain quotes, backflash or whitespace');
-      }
+      expect(() => ValidateUser.validatePassword(password)).toThrow(ValidatePasswordError);
+      expect(() => ValidateUser.validatePassword(password)).toThrow(
+        'Password cannot contain quotes, backflash or whitespace'
+      );
+      expect(spy).toHaveBeenCalledWith(password);
     });
 
-    it('Expected Sucess', () => {
+    it('Expected Success', () => {
+      const spy = jest.spyOn(ValidateUser, 'validatePassword');
       const password = 'th1s!sPASSWORD';
-
       expect(() => ValidateUser.validatePassword(password)).not.toThrow();
+      expect(spy).toHaveBeenCalledWith(password);
     });
   });
 });
