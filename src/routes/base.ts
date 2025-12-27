6fd401c6008b16f60eb8593354d942d8d59ae2e1
Triet Pham <pmtriet1211@gmail.com>
2025-12-28 00:34:47 +0200

    TSD-14 import usecase + controller to base.ts

--- src/routes/base.ts
+++ src/routes/base.ts
@@ -5,12 +5,16 @@ import { LoginUserController } from '../useCases/loginUser/loginUserController';
 import { HttpRequest } from '@useCases/common';
 import { RegisterUserController } from '@useCases/registerUser/registerUserController';
 import { RegisterUserUseCase } from '@useCases/registerUser/registerUserUseCase';
+import { LoginGoogleUserUseCase } from '@useCases/loginGoogleUser/loginGoogleUserUseCase';
+import { LoginGoogleUserController } from '@useCases/loginGoogleUser/loginGoogleUserController';
 
 const userRepo = new SQLUserRepo();
 const loginUserUseCase = new LoginUserUseCase(userRepo);
 const loginUserController = new LoginUserController(loginUserUseCase);
 const registerUserUseCase = new RegisterUserUseCase(userRepo);
 const registerUserController = new RegisterUserController(registerUserUseCase);
+const loginGoogleUserUseCase = new LoginGoogleUserUseCase(userRepo);
+const loginGoogleUserController = new LoginGoogleUserController(loginGoogleUserUseCase);
 
 export default async function baseRoutes(fastify: FastifyInstance) {
   fastify.post('/login', async (request: HttpRequest, reply: FastifyReply) => {
@@ -22,6 +26,11 @@ export default async function baseRoutes(fastify: FastifyInstance) {
   fastify.post('/register', async (request: HttpRequest, reply: FastifyReply) => {
     const controllerResponse = await registerUserController.execute(request);
 
+    return reply.status(controllerResponse.statusCode).send(controllerResponse.data);
+  });
+
+  fastify.post('/login/google', async (request: HttpRequest, reply: FastifyReply) => {
+    const controllerResponse = await loginGoogleUserController.execute(request);
     return reply.status(controllerResponse.statusCode).send(controllerResponse.data);
   });
 }
