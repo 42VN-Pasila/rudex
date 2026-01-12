a12a57809d286d4415b56f6de68aa493b7b069a8
Siuol <nguyencaotan03072000@gmail.com>
2026-01-12 11:51:00 +0200

    Update refresh Token UseCase

--- src/domain/error/tokenError.ts
+++ src/domain/error/tokenError.ts
@@ -3,7 +3,8 @@ import { BaseError } from './baseError';
 export class InvalidTokenError extends BaseError {
   public readonly code: string = 'INVALID_TOKEN';
 
-  static create(): InvalidTokenError {
-    return new this('Unauthorized', 'Invalid or expired token is founded');
+  static create(message?: string): InvalidTokenError {
+    if (!message) return new this('Unauthorized', 'Invalid or expired token is founded');
+    return new this('Unauthorized', message);
   }
 }
--- src/useCases/refreshToken/refreshTokenUseCase.ts
+++ src/useCases/refreshToken/refreshTokenUseCase.ts
@@ -1,12 +1,52 @@
 import { InvalidTokenError } from '@domain/error/tokenError';
 import { IRefreshTokenResponse } from './refreshTokenResponse';
-import { IBaseUseCase, Result } from '@useCases/common';
+import { err, IBaseUseCase, Result } from '@useCases/common';
 import { IRefreshTokenRequest } from './refreshTokenRequest';
+import { UserNotFoundError } from '@domain/error';
+import { IUserRepo } from '@repository/interfaces/userRepo';
+import { signJwt, verifyJwt } from '@services/jwt/jwt';
+import { JWT_ACCESS_TOKEN_EXP } from '@src/constants';
 
-export type IResponse = Result<IRefreshTokenResponse, InvalidTokenError>;
+const ONE_HOUR_IN_SECONDS = 3600;
+
+export type IResponse = Result<IRefreshTokenResponse, InvalidTokenError | UserNotFoundError>;
 
 export type IRefreshTokenUseCase = IBaseUseCase<IRefreshTokenRequest, IResponse>;
 
 export class RefreshTokenUseCase implements IRefreshTokenUseCase {
-    
+  private readonly userRepo: IUserRepo;
+
+  constructor(userRepo: IUserRepo) {
+    this.userRepo = userRepo;
+  }
+
+  async execute(request?: IRefreshTokenRequest): Promise<IResponse> {
+    if (!request) {
+      return err(InvalidTokenError.create('Token not found'));
+    }
+
+    const { refreshToken } = request;
+
+    try {
+      const decoded = await verifyJwt(refreshToken);
+
+      if (!decoded || typeof decoded !== 'object') return err(InvalidTokenError.create());
+
+      const userId = (decoded as any).userId;
+      const exp = (decoded as any).exp;
+
+      if (!userId) return err(InvalidTokenError.create('Invalid payload token'));
+
+      const user = this.userRepo.getById(userId);
+
+      if (!user) return err(UserNotFoundError.create('User not found'));
+
+
+      const newAccessToken = await signJwt({userId: userId}, JWT_ACCESS_TOKEN_EXP);
+      const accessTokenExpiryDate = new Date(Date.now() + JWT_ACCESS_TOKEN_EXP*1000);
+
+      
+
+    } catch (error: any) {}
+  }
 }
