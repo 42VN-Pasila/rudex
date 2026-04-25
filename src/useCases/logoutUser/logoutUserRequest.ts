b02222d3cbb72bb71c5f420fcbca35cd77919e41
TRIET PHAM <106890020+PHAM-MINH-TRIET@users.noreply.github.com>
2026-04-25 15:10:25 +0300

    remove unused comments

--- src/useCases/logoutUser/logoutUserRequest.ts
+++ src/useCases/logoutUser/logoutUserRequest.ts
@@ -1,4 +1,3 @@
-// Update this type to an object (e.g., { refreshToken: string }) when adding more business logic
 export interface LogoutUserRequest {
   username?: string;
 }
--- src/useCases/logoutUser/logoutUserResponse.ts
+++ src/useCases/logoutUser/logoutUserResponse.ts
@@ -1,2 +1,1 @@
-// Update this type to an object if the UseCase needs to return data in the future
 export type LogoutUserResponse = void;
